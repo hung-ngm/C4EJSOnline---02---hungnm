dbbdea4c4145b2261366e41c00914e0446f60914
minhhung123 <prohungnguyenminh@gmail.com>
2020-05-14 15:21:06 +0700

    Exercise 2

--- homework4.js
+++ homework4.js
@@ -1,13 +1,27 @@
 //Exercise 1
-const product = {
-    name: 'Xiaomi rice cooker',
-    price: 1700,
-    brand: 'Xiaomi',
-    color:'white'
-};
+// const product = {
+//     name: 'Xiaomi rice cooker',
+//     price: 1700,
+//     brand: 'Xiaomi',
+//     color:'white'
+// };
 
-for (let x in product){
-    console.log(`${x} : ${product[x]}`);
-}
+// for (let x in product){
+//     console.log(`${x} : ${product[x]}`);
+// }
 
 // x receives Property from product
+
+
+//Exercise 2
+const task = {
+    subject: 'Implement login feature',
+    createdBy: 'Hoang Ngoc Duc',
+    assignTo: 'Nguyen Phuong Nam',
+    dueDate:  `2019-10-08T18:00:24+0000`,
+    expectedHours: 0.5,
+};
+({subject, dueDate, assignTo} = task);
+console.log(subject);
+console.log(dueDate);
+console.log(assignTo);

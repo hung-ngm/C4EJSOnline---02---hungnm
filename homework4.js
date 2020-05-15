5b8805b3a5af62e4cd675b12626e80765cfb1e44
minhhung123 <prohungnguyenminh@gmail.com>
2020-05-15 16:42:05 +0700

    Exercise 10

--- homework4.js
+++ homework4.js
@@ -1168,23 +1168,123 @@
 // }
 
 //Exercise 9
-const rect = {
-    x: 100,
-    y: 50,
-    width: 20,
-    height: 40,
-};
-clear();
-penup();
-rt(90);
-fd(rect.x);
-lt(90);
-fd(rect.y);
-pendown();
-for(let i = 0; i<2; i++){
-  fd(rect.height);
-  rt(90);
-  fd(rect.width);
-  rt(90);
+// const rect = {
+//     x: 100,
+//     y: 50,
+//     width: 20,
+//     height: 40,
+// };
+// clear();
+// penup();
+// rt(90);
+// fd(rect.x);
+// lt(90);
+// fd(rect.y);
+// pendown();
+// for(let i = 0; i<2; i++){
+//   fd(rect.height);
+//   rt(90);
+//   fd(rect.width);
+//   rt(90);
 
+// }
+
+//Exercise 10
+const cmds = [
+    {
+        shape: 'square',
+        x: 20,
+        y: 40,
+        width: 50,
+    },
+    {
+        shape: 'rect',
+        x: 8,
+        y: 70,
+        width: 12,
+        height: 40,
+    },
+    {
+        shape: 'rect',
+        x: 70,
+        y: 70,
+        width: 12,
+        height: 40,
+    },
+    {
+        shape: 'rect',
+        x: 10,
+        y: 20,
+        width: 70,
+        height: 20,
+    },
+    {
+        shape: 'square',
+        x: 25,
+        y: 40,
+        width: 14,
+    },
+    {
+        shape: 'square',
+        x: 33,
+        y: 42,
+        width: 6,
+    },
+    {
+        shape: 'square',
+        x: 50,
+        y: 40,
+        width: 14,
+    },
+    {
+        shape: 'square',
+        x: 58,
+        y: 42,
+        width: 6,
+    },
+    {
+        shape: 'square',
+        x: 40,
+        y: 25,
+        width: 8,
+    },
+    {
+        shape: 'square',
+        penWidth: 1,
+        x: 500,
+        y: 500,
+        width: 15,
+    },
+]
+for(let i = 0; i < cmds.length; i++){
+    if(cmds[i].shape == 'square'){
+        penup();
+        rt(90);
+        fd(cmds[i].x);
+        lt(90);
+        fd(cmds[i].y);
+        pendown();
+        for(let j = 0; j < 4; j++){
+          fd(cmds[i].width);
+          rt(90);
+        }
+        home();
+    }
+    if(cmds[i].shape = 'rect'){
+        penup();
+        rt(90);
+        fd(cmds[i].x);
+        lt(90);
+        fd(cmds[i].y);
+        pendown();
+        for(let l = 0; l<2; l++){
+          fd(cmds[i].height);
+          rt(90);
+          fd(cmds[i].width);
+          rt(90);
+
+        }
+        home();
+
+    }
 }

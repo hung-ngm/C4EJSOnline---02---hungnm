52c3eb3765feb3252ab0a790b10bb32c34cd5765
minhhung123 <prohungnguyenminh@gmail.com>
2020-05-15 16:32:17 +0700

    Exercise 9

--- homework4.js
+++ homework4.js
@@ -1150,19 +1150,41 @@
 // fd(pos.y);
 
 //Exercise 8
-const square = {
+// const square = {
+//     x: 100,
+//     y: 50,
+//     width: 20,
+// };
+// clear();
+// penup();
+// rt(90);
+// fd(square.x);
+// lt(90);
+// fd(square.y);
+// pendown();
+// for(let i = 0; i < 4; i++){
+//   fd(square.width);
+//   rt(90);
+// }
+
+//Exercise 9
+const rect = {
     x: 100,
     y: 50,
     width: 20,
+    height: 40,
 };
 clear();
 penup();
 rt(90);
-fd(square.x);
+fd(rect.x);
 lt(90);
-fd(square.y);
+fd(rect.y);
 pendown();
-for(let i = 0; i < 4; i++){
-  fd(square.width);
+for(let i = 0; i<2; i++){
+  fd(rect.height);
   rt(90);
+  fd(rect.width);
+  rt(90);
+
 }

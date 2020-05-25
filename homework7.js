9669b5bb3ed47eab1fcc9a70fa109f37706ca2f0
minhhung123 <prohungnguyenminh@gmail.com>
2020-05-25 21:10:34 +0700

    Exercise 3

--- homework7.js
+++ homework7.js
@@ -1,4 +1,12 @@
+
+//Exercise 1
 const container = document.getElementById('list');
 for(let i = 0; i < 3; i++){
     container.insertAdjacentHTML('beforeend',`<li>${i}</li>`);
 }
+
+//Exercise 3
+//A variable declared by let, in a function cannot exist after the function finishes running.
+//A variable declared by var, in a function cannot exist after the function finishes running.
+
+//Exercise 4
